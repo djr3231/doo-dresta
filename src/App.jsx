91fc1178bfd82b5c23335116159ab1f386086889
Your name <you@example.com>
2023-04-09 20:27:01 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <p>hii</p>
+    
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [ "./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+});
