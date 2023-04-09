e9a9f8271a6ffc9c089164a7d9b79555666bd780
Your name <you@example.com>
2023-04-09 21:38:06 +0300

    fgdff

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { createBrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import Layout from '../layout/Layout';
+import LogIn from '../component/LogIn';
+import SingUp from '../component/SingUp';
 
 const AppRoutes = () => {
 
@@ -12,6 +14,8 @@ const AppRoutes = () => {
             <Route path='/' element={<Layout/>}>
                      
                 <Route index element={<Home/>}/>
+                <Route path='/LogIn' element={<LogIn/>}/>
+                <Route path='/SingIn' element={<SingUp/>}/>
 
             
 
