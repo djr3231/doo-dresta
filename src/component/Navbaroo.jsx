ebcd9a246f55fbd2400ccf042f2aae910d080039
Your name <you@example.com>
2023-04-09 21:59:56 +0300

    dfd

--- src/component/Navbaroo.jsx
+++ src/component/Navbaroo.jsx
@@ -5,7 +5,7 @@ const Navbaroo = () => {
   return (
     <div className="navbar bg-base-100">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <Link to='/' className="btn btn-ghost normal-case text-xl">Doo-D</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
--- src/component/SingUp.jsx
+++ src/component/SingUp.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const SingUp = () => {
   return (
-    <div>SingUp</div>
+    <div className="h-screen bg-gradient-to-br from-pink-300 to-indigo-700 flex justify-center items-center w-full">
+  <form >
+    <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
+      <div className="space-y-4">
+        <h1 className="text-center text-2xl font-semibold text-gray-600">Register</h1>
+        <div>
+          <label className="block mb-1 text-gray-600 font-semibold">Username</label>
+          <input  type="text" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+        </div>
+        <div>
+          <label className="block mb-1 text-gray-600 font-semibold">Email</label>
+          <input  type="text" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+        </div>
+        <div>
+          <label className="block mb-1 text-gray-600 font-semibold">Password</label>
+          <input  type="password" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+        </div>
+        <div>
+          <label className="block mb-1 text-gray-600 font-semibold">Confirm Password</label>
+          <input  type="password" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+        </div>
+      </div>
+      <button className="mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide">Register</button>
+      <button  className="mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide">Reset</button>
+    </div>
+  </form>
+</div>
   )
 }
 
