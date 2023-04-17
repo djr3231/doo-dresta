debdeea86eec7a615afa8395a7850385df123896
Your name <you@example.com>
2023-04-17 18:19:53 +0300

    cdcd

--- src/component/Navbaroo.jsx
+++ src/component/Navbaroo.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const Navbaroo = () => {
   return (
     <div className="navbar bg-base-100">
   <div className="flex-1">
-    Doo-D
+    <Link to='/' className="btn btn-ghost normal-case text-xl">Doo-D</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
+      <li><Link to='/Counter'>Counter</Link></li>
+      <li><Link to='/SingUp'>SingUp</Link></li>
+      <li><Link to='/LogIn'>LogIn</Link></li>
+      <li><Link to='/Admin'>Admin</Link></li>
     </ul>
   </div>
 </div>
